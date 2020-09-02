d5e48af215e1de2e333a56bdcb8687b094e02a9a
Kyle Johnson <KJohnson3288@yahoo.com>
2020-09-01 22:39:34 -0400

    initial commit

---
+++ models/workout.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose'); 
+
+const Schema = mongoose.Schema; 
+
+const workoutSchema = new Schema(
+    {
+        day: {
+          type: Date,
+          default: Date.now
+        },
+        exercises: [
+          {
+            type: {
+              type: String,
+              trim: true,
+              required: "Please enter the type of exercise?"
+            },
+            name: {
+              type: String,
+              trim: true,
+              required: "Please enter the name of the exercise?"
+            },
+            duration: {
+              type: Number,
+              required: "Please enter the time of the exercise?"
+            },
+            weight: {
+              type: Number
+            },
+            reps: {
+              type: Number
+            },
+            sets: {
+              type: Number
+            },
+            distance: {
+              type: Number
+            }
+          }
+        ]
+      }
+); 
+
+const Workout = mongoose.model("workout", workoutSchema);
+
+module.exports = Workout;
